f78bac6242181ff0a44a8beecee4c48284f8f1a7
Mugisha Gentil <gentilmugisha178@gmail.com>
2024-06-01 11:56:27 +0200

    new commit

--- index.js
+++ index.js
@@ -60,6 +60,65 @@ console.log(greet("Gentil"))
 function sliceString(arr){
   if(arr.length % 2 === 0){
     return arr.slice(0, arr.length /2)
-  }return arr
+  }return arr;
 }
 console.log(0, sliceString("Gentil"))
+
+
+function greaterThan4(x){
+  return x > 4
+}
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(greaterThan4(number))
+// square of all elements in a given array
+
+const array = [2, 5, 9];
+let squares = array.map((num) => num * num);
+
+console.log(squares); // [4, 25, 81]
+console.log(array); // [2, 5, 9]
+
+// Extracting values from an array of objects
+
+const girls = [
+  {name: 'Sarah', age: 19},
+  {name: 'Laura', age: 10},
+  {name: 'Jessy', age: 29},
+  {name: 'Amy', age: 23}];
+
+let girlsAges = girls.map((girl) => girl.age);
+
+console.log(girlsAges);  //[19, 10, 29, 23]
+
+//  Apply the Callback on only certain elements
+
+const numbers = [4, 9, 36, 49];
+const numfunction = numbers.map(num => {
+  if(num % 2 !=0){
+    return Math.sqrt(num)
+  }
+  return num
+})
+console.log(numfunction);
+
+// The callback can have side effects.
+
+const cart = [5, 15, 25];
+let total = 0;
+const withTax = cart.map((cost) => {
+  total += cost;
+  return cost * 1.2;
+});
+console.log(withTax); // [6, 18, 30]
+console.log(total); // 45
+
+// Find evenNumber and multply it by2
+
+let mult = 0;
+const numbers = [4,6,10,];
+const EvenNum = numbers.map(num => {
+  if(num % 2 === 0){
+    mult = num
+  }return mult * 2
+})
+console.log(EvenNum)
